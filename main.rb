ab4d3c5169cfeb48d7ede3b400169c67e1ebb036
Mike Rachman <mikerachman1@gmail.com>
2022-06-02 14:24:39 -0400

    Added create_code method

--- main.rb
+++ main.rb
@@ -7,8 +7,17 @@ class MasterMind
     @numbers = [1, 2, 3, 4, 5, 6]
     @guesses = 12
   end
-
+  
+  #private
+  def create_code
+    secret_code = []
+    4.times do
+        num = rand(0..5)
+        secret_code.push(@numbers[num])
+    end
+    puts secret_code
+  end
 end
 
 game = MasterMind.new
-game
+game.create_code
